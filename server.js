cc2009da8abac4f3fbc4b0c061fe9b145ab84291
= <=>
2024-01-21 20:47:14 +0500

    commit

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 import 'express-async-errors';
 import * as dotenv from 'dotenv';
-dotenv.config();
 import express from 'express';
-const app = express();
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import cloudinary from 'cloudinary';
 import helmet from 'helmet';
-import mongoSanitize from 'express-mongo-sanitize';
+import mongSanitize from 'express-mongo-sanitize';
 
 // routers
 import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
-import userRouter from './routes/userRouter.js';
-// public
+import userRoute from './routes/userRoute.js';
+
+// middleware
+import errorHandlerMiddlewares from './middlewares/errorHandlerMiddleware.js';
+import { authenticateUser } from './middlewares/authMiddleware.js';
+
+// uploads
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-// middleware
-import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
-import { authenticateUser } from './middleware/authMiddleware.js';
+dotenv.config();
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
 cloudinary.config({
 	cloud_name: process.env.CLOUD_NAME,
@@ -30,43 +36,35 @@ cloudinary.config({
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
-}
+
 app.use(express.static(path.resolve(__dirname, './client/dist')));
-app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
-app.use(mongoSanitize());
+app.use(mongSanitize());
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-	res.send('Hello World');
-});
-
-app.get('/api/v1/test', (req, res) => {
-	res.json({ msg: 'test route' });
-});
-
+//Use router
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
-app.use('/api/v1/users', authenticateUser, userRouter);
+app.use('/api/v1/users', authenticateUser, userRoute);
 app.use('/api/v1/auth', authRouter);
 
-app.get('*', (req, res) => {
+app.use('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 app.use('*', (req, res) => {
-	res.status(404).json({ msg: 'not found' });
+	res.status(404).json({ message: 'not found' });
 });
 
-app.use(errorHandlerMiddleware);
+
+app.use(errorHandlerMiddlewares);
 
 const port = process.env.PORT || 5100;
 
 try {
 	await mongoose.connect(process.env.MONGO_URL);
 	app.listen(port, () => {
-		console.log(`server running on PORT ${port}...`);
+		console.log(`server running on PORT ${port}`);
 	});
 } catch (error) {
 	console.log(error);
